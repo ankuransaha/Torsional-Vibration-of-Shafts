052011f5accabdd380f5a073ff92f32ed28b61f3
ankuransaha <121350252+ankuransaha@users.noreply.github.com>
2025-08-25 13:43:05 +0530

    Update rotor_freq.py

--- rotor_freq.py
+++ rotor_freq.py
@@ -5,7 +5,7 @@ def two_rotor(L,Ia,Ib,ds,G):
   J=(pi*ds**4)/32
   la=(Ib/(Ia+Ib))*L
   lb=(Ia/(Ia+Ib))*L
-  fna=(1/(2*pi))*(G*J/(Ia*la))**0.5
+  fna=(1/(2*pi))*math.sqrt(G*J/(Ia*la))
   fnb=(1/(2*pi))*(G*J/(Ib*lb))**0.5
   return fna
 """[link text](https://) https://github.com/ankuransaha/Torsional-Vibration-of-Shafts.git"""
